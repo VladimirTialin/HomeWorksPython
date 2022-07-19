61ac826549dafa0bef13ee34874da1e6fb4485eb
VladimirTialin <103684564+VladimirTialin@users.noreply.github.com>
2022-07-19 22:12:36 +0300

    Решили задачу 2

--- HomeWork3/Task2.py
+++ HomeWork3/Task2.py
@@ -6,20 +6,29 @@
     список: ["йцу", "фыв", "ячс", "цук", "йцукен"], ищем: "йцу", ответ: -1
     список: ["123", "234", 123, "567"], ищем: "123", ответ: -1
     список: [], ищем: "123", ответ: -1
+    
+    
+    {"Список1": ["qwe", "asd", "zxc", "qwe", "ertqwe"], "Список2":
+["йцу", "фыв", "ячс", "цук", "йцукен", "йцу"], "Список3":
+["йцу", "фыв", "ячс", "цук", "йцукен"], "Список4":
+["123", "234", 123, "567"], "Список5": []}
 '''
 import json
+
 def FindPositionStr(list):
-    findWord=input('Введите искомое слово: ')
+    findWord=input(' ищем: ')
     try:
         for i in range(len(list)):
             if findWord==list[i]:
                 result=i
                 if result==0: result=-1
-        return f': {list}, ищем {findWord}, ответ: {result}'
+        return f'Ответ: слово "{findWord}" находится на позиции списка по индексом [{result}]\n'
     except:
-        return f': {list}, ищем {findWord}, ответ: -1'
+        return f'Ответ: слово "{findWord}" находится на позиции списка по индексом [-1]\n'
 UserLibrary={}
-with open('spisok.json', 'r', encoding='utf-8') as f:
+with open('C:/Users/Владимир/Desktop/Python/HomeWorks/HomeWork3/data.json', 'r', encoding='utf-8') as f:
     UserLibrary = json.load(f)
-for x,y in UserLibrary.items():
-    print(x,FindPositionStr(y))
+
+    for x,y in UserLibrary.items():
+        print(x,y,end='')
+        print(FindPositionStr(y))
