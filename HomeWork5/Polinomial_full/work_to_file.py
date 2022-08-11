4d40c9539ac72cc34dd8fdf5332d4583a513b26d
VladimirTialin <103684564+VladimirTialin@users.noreply.github.com>
2022-08-12 00:29:05 +0300

    Delete HomeWork5 directory

--- HomeWork5/Polinomial_full/work_to_file.py
+++
@@ -1,9 +0,0 @@
-def ImportFile(name):
-    try:
-        with open(name, 'r') as file:
-            return file.read()
-    except:
-        print('Ошибка чтения файла! Файл не найден!')
-def ExportFile(result):
-    with open('F:/Python/HomeWorks/HomeWork5/Polinomial_full/sumPolinomial.txt', 'w+') as file:
-     return file.write(result)
